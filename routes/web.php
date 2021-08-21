50bc855e06807cf698a4960751439f16f8fe199e
RamRam <60738983+dev-makanju@users.noreply.github.com>
2021-08-21 18:36:58 +0100

    completed the home route

--- routes/web.php
+++ routes/web.php
@@ -1,2 +1,19 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\pagesController;
+use App\Http\Controllers\Auth\RegisterController;
+
+
+
+Route::get('/', [pagesController::class, 'getIndexPage']);
+Route::get('/contact', [pagesController::class, 'getContactPage'])->name('pages.contact');
+Route::get('/faq', [pagesController::class, 'getFaqPage'])->name('pages.faq');
+Route::get('/about', [pagesController::class, 'getAboutPage'])->name('pages.about');
+Route::get('/register', [RegisterController::class, 'register'])->name('Auth.register');
+Route::get('/login', [RegisterController::class, 'login'])->name('Auth.login');
+
+
+
+
+
